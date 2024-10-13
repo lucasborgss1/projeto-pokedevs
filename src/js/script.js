34c66db71ec44957e9eac09e32d7998b7e397efe
Lucas de Almeida Borges <153786647+lucasborgss1@users.noreply.github.com>
2024-10-13 16:07:12 -0300

    Projeto

---
+++ src/js/script.js
@@ -0,0 +1,33 @@
+const listaSelecaoPokevs = document.querySelectorAll(".pokedev");
+
+listaSelecaoPokevs.forEach(pokedev => {
+  pokedev.addEventListener("click", () => {
+    esconderCartaoPokedev();
+    const idPokedevSelecionado = mostrarCartaoPokedev(pokedev);
+    desativarPokedevNaListagem();
+    ativarPokedevNaListagem(idPokedevSelecionado);
+  })
+})
+
+function ativarPokedevNaListagem(idPokedevSelecionado) {
+  const pokedevSelecionadoNaListagem = document.getElementById(idPokedevSelecionado);
+  pokedevSelecionadoNaListagem.classList.add("ativo");
+}
+
+function desativarPokedevNaListagem() {
+  const pokedevAtivoNaListagem = document.querySelector(".ativo");
+  pokedevAtivoNaListagem.classList.remove("ativo");
+}
+
+function mostrarCartaoPokedev(pokedev) {
+  const idPokedevSelecionado = pokedev.attributes.id.value;
+  const idDoCartaoPokedevParaAbrir = "cartao-" + idPokedevSelecionado;
+  const cartaoPokedevParaAbrir = document.getElementById(idDoCartaoPokedevParaAbrir);
+  cartaoPokedevParaAbrir.classList.add("aberto");
+  return idPokedevSelecionado;
+}
+
+function esconderCartaoPokedev() {
+  const cartaoPokedevAberto = document.querySelector(".aberto");
+  cartaoPokedevAberto.classList.remove("aberto");
+}
